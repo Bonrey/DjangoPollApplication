2049c7537e79536423965648d440a68cc8821f25
Bonrey <m.vologodskiy@gmail.com>
2022-01-18 12:50:33 +0300

    Lesson 2

--- DjangoPollApplication/settings.py
+++ DjangoPollApplication/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = "DjangoPollApplication.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "poll_application",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "Mastery",
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
